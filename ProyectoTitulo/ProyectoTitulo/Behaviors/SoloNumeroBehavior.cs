fc5f50c02457e806b333733a551021a5a97ae247
Luciano <95.luciano.alvares@gmail.com>
2018-10-21 18:47:12 -0300

    Behaviors
    Se agrego behaviors para validar correo e ingreso de solo numeros

---
+++ ProyectoTitulo/ProyectoTitulo/Behaviors/SoloNumeroBehavior.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ProyectoTitulo.Behaviors
+{
+    public class SoloNumeroBehavior : Behavior<Entry>
+    {
+        protected override void OnAttachedTo(Entry cajaTexto)
+        {
+            cajaTexto.TextChanged += CambiaTexto;
+            base.OnAttachedTo(cajaTexto);
+        }
+        protected override void OnDetachingFrom(Entry cajaTexto)
+        {
+            cajaTexto.TextChanged -= CambiaTexto;
+            base.OnDetachingFrom(cajaTexto);
+        }
+        private void CambiaTexto(object sender, TextChangedEventArgs args)
+        {
+            bool valido = int.TryParse(args.NewTextValue, out int resultado);
+            ((Entry)sender).TextColor = valido ? Color.Default : Color.Red;
+        }
+
+    }
+}
