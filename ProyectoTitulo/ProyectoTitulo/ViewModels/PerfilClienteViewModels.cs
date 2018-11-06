80368c26b1731b6b2c9d8c323cc0e43d8da20680
Luciano <95.luciano.alvares@gmail.com>
2018-11-05 22:45:08 -0300

    Update ProyectoTitulo/ProyectoTitulo/ViewModels/PerfilClienteViewModels.cs

---
+++ ProyectoTitulo/ProyectoTitulo/ViewModels/PerfilClienteViewModels.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using ProyectoTitulo.ViewModels;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+
+namespace ProyectoTitulo.ViewModels
+{
+  
+    public class ClientesModel
+    {
+
+        public ObservableCollection<ClientesModel> clientes { get; set; }
+
+        public ClientesModel()
+        {
+            if(clientes==null)
+            {
+                clientes = new ObservableCollection<ClientesModel>();
+            }
+        }
+
+        public ObservableCollection<ClientesModel> Consultar()
+        {
+            return clientes;
+
+        }
+        public void AgregarCliente(ClientesModel modelo)
+        {
+            clientes.Add(modelo);
+
+        }
+    }
+}
